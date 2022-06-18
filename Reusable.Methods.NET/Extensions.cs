def9aca3f4e40626b792798e4d5479f2c8f510b4
nicober.mani <nicobermani@outlook.com>
2022-06-18 23:25:44 +0530

    Added Methods

---
+++ Reusable.Methods.NET/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Reusable.Methods.NET
+{
+    public static partial class Extensions
+    {
+
+        public static string GetRuntimeEnvironmentGetRuntimeDirectory()
+        {
+            return RuntimeEnvironment.GetRuntimeDirectory();
+        }
+
+    }
+}
