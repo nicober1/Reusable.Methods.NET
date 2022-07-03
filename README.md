2fe71b43799223483c1d10051a9d4e3ab315907b
nicober1 <85498722+nicober1@users.noreply.github.com>
2022-07-03 07:43:30 +0000

    ffffffffff

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Reusable.Methods.NET
+# Reusable.Methods.NET Nuget Package - This Project/Nuget Package contains C# Extension Methods which are useful and help in increasing productivity saving valuable time.
+
+# Owner and Creator : Nicober Mani
---
+++ Reusable.Methods.NET/Reuse.cs
@@ -0,0 +1,43 @@
+﻿using Bogus;
+
+namespace Reusable.Methods.NET
+{
+    public static partial class Reuse
+    {
+        public static bool IsRunningMocked { get; set; }
+        public static Action<string> LoggerMethod { get; set; }
+
+        public static string ProjectPath { get; set; }
+
+        static Reuse()
+        {
+            LoggerMethod = Console.WriteLine;
+            ProjectPath = ".";
+        }
+
+        public static void Log(string message)
+        {
+            LoggerMethod.Invoke(message);
+        }
+
+        public static void Log(object obj)
+        {
+            if (obj != null)
+            {
+                LoggerMethod.Invoke(obj.ToString());
+            }
+            else
+            {
+                LoggerMethod.Invoke("(null)");
+            }
+        }
+
+        public static void Log()
+        {
+            Reuse.Log("");
+        }
+
+       
+
+    }
+}
